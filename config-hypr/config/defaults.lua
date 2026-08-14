21959f50c797ff28ee4bd778c9544024c977d959
Martin Goldhahn <martin@goldhahn.info>
2026-08-14 19:11:46 +0200

    copy lappi files to git repo

---
+++ config-hypr/config/defaults.lua
@@ -0,0 +1,9 @@
+-- Hyprland default apps
+
+TERMINAL     = "alacritty"
+FILE_MANAGER = "dolphin"
+BROWSER      = "vivaldi"
+EDITOR       = "gnome-text-editor --new-window"
+CALCULATOR   = "gnome-calculator"
+PASSWORDMGR  = "1password"
+IDEA         = "idea"
